ccbd67a1439de678db786456ed81d25eb1aaf93a
zazimov <zalmanazimov@pursuit.org>
2023-03-31 02:42:41 -0400

    Created character images and tags along with html skeleton

--- app.js
+++ app.js
@@ -1,9 +1,47 @@
 const form = document.querySelector(".search-form");
 
-async function getWeatherInfo(base_URL) {
-  const response = await fetch(
-    `https://rickandmortyapi.com/api/character${char_num}`
-  );
-  const parsedData = await response.json();
-  return parsedData;
+async function fetchData(inputName) {
+  try {
+    const response = await fetch(inputName);
+    const parsedData = await response.json();
+    return parsedData;
+  } catch (error) {
+    console.log(error);
+  }
 }
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  fetchData(
+    `https://rickandmortyapi.com/api/character/?name=${event.target.character.value}`
+  ).then((ele) => {
+    // console.log(ele);
+    // console.log(ele.results);
+    // console.log(ele.results[0].name);
+
+    let arr = ele.results;
+    nameArr(arr);
+  });
+});
+
+function nameArr(array) {
+  array.forEach((characterObj) => {
+    console.log(characterObj.name);
+    const charName = characterObj.name;
+    const mainTag = document.querySelector("main");
+    const divTag = document.createElement("div");
+    divTag.setAttribute("class", "character-name");
+    divTag.textContent = charName;
+    const imgTag = document.createElement("img");
+    imgTag.setAttribute("src", characterObj.image);
+    imgTag.setAttribute("alt", `Picture of ${characterObj.name}`);
+    divTag.appendChild(imgTag);
+    mainTag.append(divTag);
+
+    // console.log(characterObj.name);
+  });
+}
+
+// console.log(nameInfo);
+
+// function nameCard()
