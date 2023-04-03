a635b747bd10f5bdd2cee0b606e28b2c1ca18818
zazimov <zalmanazimov@pursuit.org>
2023-04-02 22:53:53 -0400

    completed webpage, fully responsive, can change reponsively to various screen sizes

--- about.js
+++ about.js
@@ -1,5 +1,6 @@
+// Creating an event that will execute a function, upon the page loading, in which it will grab the gif from the about.html and append it to the div tag , class container2 and will set a timer for it to disappear after 1.55 seconds. This gif of the portal dumping out Rick and the kids is my transition to being loaded or "dumped" onto a new page!
 window.addEventListener("load", function () {
-  var imageElement = document.getElementById("my-image");
+  let imageElement = document.getElementById("my-image");
   imageElement.src = "rickandomortygifPORTAL.gif";
 });
 
--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const form = document.querySelector(".search-form");
 
+// Creating a function that will do a fetch
 async function fetchData(inputName) {
   try {
     const response = await fetch(inputName);
@@ -10,39 +11,44 @@ async function fetchData(inputName) {
   }
 }
 
+// Creating an event, submit type
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
+  // Performing a fetch
   fetchData(
     `https://rickandmortyapi.com/api/character/?name=${event.target.character.value}`
   ).then((ele) => {
+    // Creating a condition that will determine whether user input is erroneous or a valid search
     if (ele.hasOwnProperty("error") && ele.error === "There is nothing here") {
+      // error search, will create a p tag with an error message and add that to the DOM, at the level of the main tag
       const pError = document.createElement("p");
       pError.textContent = "Error -- There is nothing here";
       const mainTag = document.querySelector("main");
       mainTag.innerHTML = "";
       mainTag.append(pError);
     } else {
+      // valid search, which will clear the contents of the main tag, and then create an unordered list tag and append it to the main tag
       const mainTag = document.querySelector("main");
       mainTag.innerHTML = "";
       const ulTag = document.createElement("ul");
       mainTag.append(ulTag);
+      // calling my nameArr function
       nameArr(ele.results);
     }
   });
 });
 
+// Creating a function that will take in an array and iterate through each element, and will take the corresponding name and image for the element, before putting them inside a newly created list item, and appending that item to an unordered list
 function nameArr(array) {
   array.forEach((characterObj) => {
-    // console.log(characterObj.name);
     const charName = characterObj.name;
+    // Creating new list item tag
     const newItem = document.createElement("li");
+    // Adding attributes of the name and image to the list item
     newItem.setAttribute("class", "character-name");
     newItem.innerHTML = `<span class="spanText">${charName}</span><img src="${characterObj.image}" alt="${characterObj.name}"/>`;
-    // const imgTag = document.createElement("img");
-    // imgTag.setAttribute("src", characterObj.image);
-    // imgTag.setAttribute("alt", `Picture of ${characterObj.name}`);
-    // newItem.appendChild(imgTag);
+    // Appending the list item to the unordered list
     const ulTag2 = document.querySelector("ul");
     ulTag2.appendChild(newItem);
   });
